9dc71ae6e487b94318bac4b2e4fbbacb2624b03f
carlos eduardo <https://carloseduardo.inforgeneses.com.br/>
2022-04-06 23:02:01 -0300

    adicionado skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,22 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillContent = document.getElementsByClassName('skills__content'),
+skillHeader= document.querySelectorAll('.skills__header')
 
+function toggeSkills(){
+    let itemClass = this.parentNode.className
+    for(i=0; i< skillContent.length; i++){
+        skillContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass ==='skills__content skills__close'){
+        this.parentNode.className= 'skills__content skills__oper'
+    }
 
+}
+skillHeader.forEach((el)=>{
+    el.addEventListener('click', toggeSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 

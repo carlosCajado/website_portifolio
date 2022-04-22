81ca48aea035c2b5c10f3768001f89b069e164ec
carlos <cajadomotos@gmail.com>
2022-04-21 21:52:55 -0300

    mod dark full

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,7 @@ function scrollTop(){
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
-window.addEventListener('scroll', scrollUp)
+// window.addEventListener('scroll', scrollUp)
 
 
 
